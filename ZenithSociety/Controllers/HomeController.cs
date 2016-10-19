b6864eb84ab252a3531ff0415d22ad2257574db6
Matthew de Asis <mattuda@gmail.com>
2016-10-18 17:05:33 -0700

    Changed Show to only display events for current week

--- ZenithSociety/Controllers/HomeController.cs
+++ ZenithSociety/Controllers/HomeController.cs
@@ -36,15 +36,31 @@ namespace ZenithSociety.Controllers
             return View();
         }
 
+        //Now displays events pertaining to this week only
         public ActionResult Show()
         {
+            var mondayOfTheWeek = getMondayOfTheWeek();
+
             var events = db.Events.Include(m => m.Activity).Include(n => n.ApplicationUser);
-            events.OrderByDescending(d => d.StartDate);
 
             ViewBag.DateFormat = "{0:dddd MMMM dd, yyyy}";
             ViewBag.TimeFormat = "{0:h:mm tt}";
 
-            return View(events.ToList());
+            return View(events.ToList().Where(d => (d.StartDate >= mondayOfTheWeek) && (d.EndDate <= mondayOfTheWeek.AddDays(7))).OrderBy(d => d.StartDate));
+        }
+
+        //Gets the monday of this week using DateTime.Now as basis
+        private DateTime getMondayOfTheWeek()
+        {
+            var currentDay = DateTime.Now;
+            var currentHour = currentDay.Hour;
+            var currentMinute = currentDay.Minute;
+            var currentSecond = currentDay.Second;
+
+            var delta = DayOfWeek.Monday - currentDay.DayOfWeek;
+
+            DateTime mondayOfTheWeek = currentDay.AddDays(delta).AddHours(-1 * currentHour).AddMinutes(-1 * currentMinute).AddSeconds(-1 * currentSecond);
+            return mondayOfTheWeek;
         }
     }
 }
